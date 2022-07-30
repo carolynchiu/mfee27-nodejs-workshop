b865151996c3967a3d239610373f70fa02dd9b7f
Carolyn <c920231@gmail.com>
2022-07-30 11:38:48 +0800

    add sum.js

---
+++ sum.js
@@ -0,0 +1,11 @@
+function sum(n) {
+  //1+2+3...+n
+  let sum = 0;
+  for (let i = 1; i <= n; i++) {
+    sum += i;
+  }
+  return sum;
+}
+console.log(sum(1));
+console.log(sum(3));
+console.log(sum(10));
