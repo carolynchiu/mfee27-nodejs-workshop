7d7e75e5a5d2728f8ec3dd0330c2c31c704cfa94
Carolyn <c920231@gmail.com>
2022-08-08 16:02:44 +0800

    update readfile-promise.js

--- homework-0806/readfile-promise.js
+++ homework-0806/readfile-promise.js
@@ -3,9 +3,35 @@ const fs = require("fs");
 // 記得要放編碼 utf8
 // callback
 // readFile 去硬碟讀檔案，這是很慢的事，他是非同步
-fs.readFile("test.txt", "utf8", (err, data) => {
-  if (err) {
-    return console.error("發生錯誤", err);
-  }
-  console.log(data);
-});
+
+function readFile(path, encoding) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, encoding, (err, data) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(data);
+    });
+  });
+}
+
+readFile("test.txt", "utf8")
+  .then((data) => {
+    // 接住resolve
+    console.log(data);
+  })
+  .catch((err) => {
+    // 用來接住 reject
+    console.error("在 promise 發生錯誤:", err);
+  });
+
+//故意犯錯 test.txt 寫成 test1.txt
+readFile("test1.txt", "utf8")
+  .then((data) => {
+    // 接住resolve
+    console.log(data);
+  })
+  .catch((err) => {
+    // 用來接住 reject
+    console.error("在 promise 發生錯誤:", err);
+  });
