11fbdb1e499e749a0e28ad232afa8cfadd58814d
Carolyn <c920231@gmail.com>
2022-08-14 12:13:19 +0800

    update crawler3.js

--- crawler/crawler3.js
+++ crawler/crawler3.js
@@ -1,6 +1,6 @@
 // 1. 自動取得今日日期 （可能利用 cron 排程工具 系統每日自動執行）
 // 2. 從檔案中讀取股票代碼
-const fs = require("fs").promises;
+const fsPromise = require("fs").promises;
 const axios = require("axios");
 const moment = require("moment");
 
@@ -13,7 +13,7 @@ let queryDate = moment().format("YYYYMMDD"); //'20220814';
 // axios.get(url, 設定)
 (async () => {
   try {
-    let stockNo = await fs.readFile("stock.txt", "utf8");
+    let stockNo = await fsPromise.readFile("stock.txt", "utf8");
     let response = await axios.get(
       `https://www.twse.com.tw/exchangeReport/STOCK_DAY`,
       {
