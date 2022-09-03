0fe2b990d0edc9239eea30f80a53435d76685c82
Carolyn <c920231@gmail.com>
2022-09-03 21:08:43 +0800

    update login

--- stock-be/routers/auth.js
+++ stock-be/routers/auth.js
@@ -2,41 +2,97 @@ const express = require("express");
 const router = express.Router();
 const pool = require("../utils/db");
 // for hash password
+// 終端機輸入指令 npm i bcrypt，接下來引用他
 const bcrypt = require("bcrypt");
+// 終端機輸入指令 npm i express-validator，接下來引用他
+const { body, validationResult } = require("express-validator");
 
-// -> /api/1.0/auth/register
-router.post("/api/1.0/auth/register", async (req, res, next) => {
-  // TODO:確認資料有沒有收到: 使用 express.json 中間件
+const registerRules = [
+  // 中間件: 檢查 email 是否合法
+  body("email").isEmail().withMessage("Email 欄位請填寫正確格式"),
+  // 中間件: 檢查密碼長度
+  body("password").isLength({ min: 8 }).withMessage("密碼長度至少為 8"),
+  // 中間件: 檢查 password & confirmPassword 是否一致
+  // 客製自己想要的條件
+  body("confirmPassword")
+    .custom((value, { req }) => {
+      return value === req.body.password;
+      // value 就是 confirmPassword
+    })
+    .withMessage("密碼驗證不一致"),
+];
+
+////// -> /api/1.0/auth/register
+router.post("/api/1.0/auth/register", registerRules, async (req, res, next) => {
+  // --- (1) 確認資料有沒有收到: 使用 express.json 中間件 (在 server.js)
   console.log("register", req.body);
-  // TODO:資料的驗證（後端不可以相信來自前端的資料）
-  // TODO:檢查 email 有沒有重複 ->不能重複
-  //      ->方法1: 交給 DB，把 email 欄位設定成 unique
-  //      ->方法2: 自己檢查，去資料庫撈 email 有沒有存在
+  // register {
+  //   email: 'carolyn55@gmail.com',
+  //   name: 'Carolyn Chiu',
+  //   password: 'testtest',
+  //   confirmPassword: 'testtest'
+  // }
+
+  // --- (2) 資料的驗證（後端不可以相信來自前端的資料）
+  const validateResult = validationResult(req);
+  console.log("validateResult", validateResult);
+  if (!validateResult.isEmpty()) {
+    // validateResult 不是空 -> 有錯誤 -> 回覆給前端
+    return res.status(400).json({ errors: validateResult.array() });
+  }
+
+  // --- (3) 檢查 email 有沒有重複 ->不能重複
+  //          -> 方法 1: 交給 DB，把 email 欄位設定成 unique
+  //          -> 方法 2: 自己檢查，去資料庫撈 email 有沒有存在
   let [member] = await pool.execute("SELECT * FROM members WHERE email = ?", [
     req.body.email,
   ]);
   console.log("member", member);
-  if (member.length === 0) {
-    // TODO:密碼要雜湊 hash
-    let hashedPassword = await bcrypt.hash(req.body.password, 10);
-    // TODO:資料存到資料庫
-    let result = await pool.execute(
-      "INSERT INTO members (email, password, name) VALUES (?, ?, ?)",
-      [req.body.email, hashedPassword, req.body.name]
-    );
-    console.log("新增會員", result);
-    // TODO:回覆前端
-    res.json({ message: "註冊成功" });
-  } else {
-    //TODO:如果 member.length !== 0 (代表這個 email 已存在資料庫中)，回覆 400 跟錯誤訊息
+
+  // --- (4) 如果 member.length > 0 (代表這個 email 已存在資料庫中)，回覆 400 跟錯誤訊息
+  if (member.length > 0) {
     return res.status(400).json({ message: "此 email 已註冊" });
   }
+
+  // --- (5) 密碼要雜湊 hash
+  let hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+  // --- (6) 把資料存到資料庫 (複習 SQL 語法)
+  let result = await pool.execute(
+    "INSERT INTO members (email, password, name) VALUES (?, ?, ?)",
+    [req.body.email, hashedPassword, req.body.name]
+  );
+  console.log("新增會員", result);
+
+  // --- (7) 回覆前端
+  res.json({ message: "註冊成功" });
+});
+
+////// -> /api/1.0/auth/login
+router.post("/api/1.0/auth/login", async (req, res, next) => {
+  console.log("login", req.body);
+  // TODO: 資料驗證
+  // 確認這個 email 有沒有註冊過
+  let [members] = await pool.execute("SELECT * FROM members WHERE email = ?", [
+    req.body.email,
+  ]);
+  if (members.length == 0) {
+    // 這個 email 沒有註冊過，就回覆 400
+    // 如果有，回覆 400 跟錯誤訊息
+    // members 的長度 == 0 -> 沒有資料 -> 這個 email 沒有註冊過
+    return res.status(400).json({ message: "帳號或密碼錯誤" });
+  }
+  let member = members[0];
+  // 有註冊過，就去比密碼
+  // (X) bcrypt.hash(req.body.password) === member.password
+  let compareResult = await bcrypt.compare(req.body.password, member.password);
+  if (!compareResult) {
+    // 如果密碼不對，就回覆 401
+    return res.status(401).json({ message: "帳號或密碼錯誤" });
+  }
+
+  // TODO: 密碼比對成功 -> (1) jwt token (2) session/cookie
+  // TODO: 回覆前端登入成功
+  res.json({ message: "登入成功" });
 });
 module.exports = router;
-
-// register {
-//   email: 'carolyn55@gmail.com',
-//   name: 'Carolyn Chiu',
-//   password: 'testtest',
-//   confirmPassword: 'testtest'
-// }
--- stock-fe/src/components/Register.js
+++ stock-fe/src/components/Register.js
@@ -8,18 +8,42 @@ const Register = () => {
     name: "Carolyn Chiu",
     password: "testtest",
     confirmPassword: "testtest",
+    // photo: "",
   });
 
+  ////// 控制(?)表單
   function handleChange(e) {
     const newMember = { ...member, [e.target.name]: e.target.value };
     setMember(newMember);
   }
 
+  ////// 上傳圖片
+  function handleUpload(e) {
+    // type=file 的 input
+    // 選好的檔案是放在 e.target.files[0]
+    setMember({ ...member, [e.target.name]: e.target.files[0] });
+  }
+
+  ////// 送出表單
   async function handleSubmit(e) {
+    // 阻止表單預設行為
     e.preventDefault();
     try {
+      // --- 方法(1): 沒有圖片上傳、單純 post 一個 json 物件
+      //           //http://localhost:3002/api/1.0
       let response = await axios.post(`${API_URL}/auth/register`, member);
+      console.log("POST res", response);
       console.log(response.data);
+
+      // --- 方法(2): 要上傳圖片 FormData
+      // let formData = new FormData();
+      // formData.append("email", member.email);
+      // formData.append("name", member.name);
+      // formData.append("password", member.password);
+      // formData.append("confirmPassword", member.confirmPassword);
+      // formData.append("photo", member.photo);
+      // let response = await axios.post(`${API_URL}/auth/register`, formData);
+      // console.log(response.data);
     } catch (e) {
       console.error("register Error", e);
     }
@@ -36,7 +60,7 @@ const Register = () => {
         </label>
         <input
           className="w-full border-2 border-purple-200 rounded-md h-10 focus:outline-none focus:border-purple-400 px-2"
-          type="text"
+          type="email"
           id="email"
           name="email"
           value={member.email}
@@ -101,11 +125,13 @@ const Register = () => {
           type="file"
           id="photo"
           name="photo"
+          onChange={handleUpload}
         />
       </div>
       <button
         className="text-xl bg-indigo-300 px-4 py-2.5 rounded hover:bg-indigo-400 transition duration-200 ease-in"
         onClick={handleSubmit}
+        type="submit"
       >
         註冊
       </button>
